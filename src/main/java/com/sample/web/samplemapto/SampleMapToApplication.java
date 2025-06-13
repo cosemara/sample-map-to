ecfa6b71bb5ea58df5fe2987b695e75b1283dfe9
jhchoi <jhchoi@atoncorp.com>
2025-06-13 19:42:03 +0900

    first commit

---
+++ src/main/java/com/sample/web/samplemapto/SampleMapToApplication.java
@@ -0,0 +1,13 @@
+package com.sample.web.samplemapto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMapToApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleMapToApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sample/web/samplemapto/SampleMapToApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.web.samplemapto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMapToApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
